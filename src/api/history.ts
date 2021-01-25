cffc16c4a300ce6580bfc927bcd5d63d22e52f1e
Willy Brauner <mail@willybrauner.com>
2021-01-25 10:44:16 +0100

    externalize history

---
+++ src/api/history.ts
@@ -0,0 +1,7 @@
+import { BrowserHistory, createBrowserHistory } from "history";
+
+/**
+ * Browser history
+ * doc: https://github.com/ReactTraining/history/blob/master/docs/getting-started.md
+ */
+export const HISTORY: BrowserHistory = createBrowserHistory();
--- src/api/routers.ts
+++ src/api/routers.ts
@@ -1,41 +1,44 @@
 import { RouterInstance, TRoute } from "./RouterInstance";
-import { BrowserHistory, createBrowserHistory } from "history";
+import { BrowserHistory } from "history";
+import { HISTORY } from "./history";
 
 export type TRoutersConfig = {
   /**
-   * Keep global routes array
+   * Global routes list
    */
   routes: TRoute[];
   /**
-   * Keep routers instances in array
+   * Routers instances list
    */
   instances: RouterInstance[];
   /**
-   * Create global browser history
-   * doc: https://github.com/ReactTraining/history/blob/master/docs/getting-started.md
+   * Global browser history
    */
   history: BrowserHistory;
   /**
-   * keep navigation history array
+   * GLobal navigation history list
    */
   locationsHistory: any[];
   /**
-   * Router counter increment in each history push
+   * Global route counter increment on each history push
    */
   routeCounter: number;
   /**
+   * Global is first route state
    * Is first route is true if routerCounter === 1
    */
   isFirstRoute: boolean;
 };
 
-const BROWSER_HISTORY: BrowserHistory = createBrowserHistory();
-
+/**
+ * ROUTERS object allows to keep safe globales values between routers instances
+ * This object values do not depend of one single router
+ */
 export const ROUTERS: TRoutersConfig = {
   routes: null,
   instances: [],
-  history: BROWSER_HISTORY,
-  locationsHistory: [BROWSER_HISTORY.location],
+  history: HISTORY,
+  locationsHistory: [HISTORY.location],
   routeCounter: 1,
   isFirstRoute: true,
 };
