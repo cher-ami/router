0ef259aabfdee802b2fa4e3006444357b98c2526
Willy Brauner <mail@willybrauner.com>
2021-03-23 14:53:35 +0100

    Improve types

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
 import * as React from "react";
 import { forwardRef } from "react";
-import { EHistoryMode, Router, TRoute } from "../src";
+import { EHistoryMode, Router, TLanguage, TRoute } from "../src";
 import { LangService, langMiddleware } from "../src";
 
 import App from "./App";
@@ -50,9 +50,10 @@ export const routesList: TRoute[] = [
 ];
 
 const baseUrl = "/master";
-const locales = [{ key: "en" }, { key: "fr" }, { key: "de" }];
 
-LangService.init(locales, true, baseUrl);
+export type TLangs = "en" | "fr" | "de";
+const languages: TLangs[] = ["en", "de", "fr"];
+LangService.init<TLangs>(languages, true, baseUrl);
 
 /**
  * Init Application
