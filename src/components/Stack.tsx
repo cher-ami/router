fb495f54c152dbbdf3cf04d227f8e121ed19896d
yoanngueny <yoann.gueny@gmail.com>
2021-04-16 11:19:51 +0200

    Fix stack component key

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -100,14 +100,14 @@ function Stack(props: IProps) {
       {previousRoute?.component && (
         <previousRoute.component
           ref={prevRef}
-          key={index - 1}
+          key={`${previousRoute.fullUrl}_${index - 1}`}
           {...(previousRoute.props || {})}
         />
       )}
       {currentRoute?.component && (
         <currentRoute.component
           ref={currentRef}
-          key={index}
+          key={`${currentRoute.fullUrl}_${index}`}
           {...(currentRoute.props || {})}
         />
       )}
