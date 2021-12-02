4c7f7c8541b8645b6b427411cb2d3354c92d7657
ciphrd <crespy.b@gmail.com>
2021-12-02 12:09:01 +0100

    extended Link props with anchor html attributes (#78)
    * extended Link props with anchor html attributes
    
    * Remove unused import
    
    * exclude "to" from props of <a> element, in Link
    
    * Rename AnchorWithoutHref to TAnchorWithoutHref type
    
    Co-authored-by: baptiste <baptiste@cher-ami.tv>
    Co-authored-by: Willy Brauner <mail@willybrauner.com>

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useMemo } from "react";
+import React, { AnchorHTMLAttributes, PropsWithChildren, useMemo } from "react";
 import { useLocation } from "..";
 import {
   createUrl,
@@ -7,17 +7,19 @@ import {
   TOpenRouteParams,
 } from "../api/helpers";
 
-interface IProps {
+// exclude href because it collides with "to"
+type TAnchorWithoutHref = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href">;
+
+export interface ILinkProps extends PropsWithChildren<TAnchorWithoutHref> {
   to: string | TOpenRouteParams;
   onClick?: () => void;
   className?: string;
-  children: ReactNode;
 }
 
 /**
  * @name Link
  */
-function Link(props: IProps) {
+function Link(props: ILinkProps) {
   const [location, setLocation] = useLocation();
 
   const url = useMemo(() => createUrl(props.to), [props.to]);
@@ -35,6 +37,7 @@ function Link(props: IProps) {
 
   return (
     <a
+      {...{ ...props, to: undefined }}
       className={joinPaths(["Link", props.className, isActive && "active"], " ")}
       onClick={handleClick}
       children={props.children}
