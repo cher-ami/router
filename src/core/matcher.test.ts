353e2bf07ff9cd06c4ebefe9142995e1898c4b0d
Willy Brauner <mail@willybrauner.com>
2021-12-09 09:49:03 +0100

    Improve matcher test

--- src/core/matcher.test.ts
+++ src/core/matcher.test.ts
@@ -2,28 +2,65 @@ import { TRoute } from "../components/Router";
 import { preventSlashes } from "./helpers";
 import { getRouteFromUrl } from "./matcher";
 
+// prettier-ignore
 const routesList: TRoute[] = [
-  { path: "/", component: null, name: "HomePage" },
-  { path: "/foo", component: null, name: "FooPage" },
-  { path: "/bar/:id", component: null, name: "BarPage", props: { color: "blue" } },
-  { path: "/:rest", component: null, name: "NotFoundPage" },
+  { path: "/", name: "HomePage" },
+  { path: "/bar/:id", name: "BarPage", props: { color: "blue" } },
+  {
+    path: "/about",
+    children: [
+      {
+        path: "/route2",
+        children: [
+          {
+            path: "/:testParam?",
+            children: [
+              {
+                 path: "/foo4" ,
+                 props: { color: "red" },
+
+              }
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ];
 const base = "/custom/base";
 
 describe("getRouteFromUrl", () => {
   it("should get right route from URL", () => {
     const getRoute = getRouteFromUrl({
-      pUrl: preventSlashes(`${base}/bar/foo`),
+      pUrl: preventSlashes(`${base}/bar/my-id`),
       pRoutes: routesList,
       pBase: base,
     });
 
-    expect(getRoute.fullUrl).toBe(`${base}/bar/foo`);
+    expect(getRoute.fullUrl).toBe(`${base}/bar/my-id`);
     expect(getRoute.fullPath).toBe(`${base}/bar/:id`);
     expect(getRoute.path).toBe("/bar/:id");
-    expect(getRoute.url).toBe("/bar/foo");
+    expect(getRoute.url).toBe("/bar/my-id");
     expect(getRoute.name).toBe(`BarPage`);
-    expect(getRoute.props).toEqual({ params: { id: "foo" }, color: "blue" });
+    expect(getRoute.props).toEqual({ params: { id: "my-id" }, color: "blue" });
+  });
+
+  it("should get right route from URL with subRoute", () => {
+    const getRoute = getRouteFromUrl({
+      pUrl: "/about/route2/super/foo4",
+      pRoutes: routesList,
+      pBase: "/",
+    });
+
+    expect(getRoute.fullUrl).toBe(`/about/route2/super/foo4`);
+    expect(getRoute.fullPath).toBe(`/about/route2/:testParam?/foo4`);
+
+    // this is the component we need to render at this level
+    expect(getRoute.path).toBe("/about");
+    expect(getRoute.url).toBe("/about");
+    // not return props {color: red }, this is the appropriate router who takes this child route
+    // witch will return it
+    expect(getRoute.props).toEqual({ params: { testParam: "super" } });
   });
 
   it("should not get route from bad URL and return undefined", () => {
