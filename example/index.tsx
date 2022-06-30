67305cddd4fca353c388687edaed93607ae98b04
Willy Brauner <mail@willybrauner.com>
2022-06-30 18:56:36 +0200

    Remove StrictMode from example

--- example/index.tsx
+++ example/index.tsx
@@ -22,7 +22,7 @@ const langService = new LangService<TLang>({
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
   <Router
     history={createBrowserHistory()}
     //staticLocation="/base/en/about"
@@ -32,5 +32,5 @@ root.render(
   >
     <App />
   </Router>
-  </React.StrictMode>
+//  </React.StrictMode>
 );
